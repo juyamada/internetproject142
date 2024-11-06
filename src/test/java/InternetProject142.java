2e753c49ad1054507b4bf6dcd8d0a2ae99d04009
juyamada <jysyamada@gmail.com>
2024-11-06 12:03:48 -0300

    ajustes espacos e comentarios

--- src/test/java/InternetProject142.java
+++ src/test/java/InternetProject142.java
@@ -1,26 +1,17 @@
 // 1 - bibliotecas e pacotes
-import static org.hamcrest.CoreMatchers.is;
-import org.junit.jupiter.api.Assertions;
-
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import dev.failsafe.internal.util.Assert;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 // 2 - classes
@@ -31,13 +22,11 @@ private WebDriver driver;
 @BeforeEach
 public void setUp() {
     driver = new ChromeDriver();
-
 } 
 
 @AfterEach
 public void tearDown() {
     driver.quit();
-
 }
 
 @Test
@@ -52,13 +41,12 @@ public void LoginComSucesso() {
     driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
     driver.findElement(By.cssSelector("i.fa.fa-2x.fa-sign-in")).click();
 
-    // Usei o assertTrue e o contains, ao inves do assertEquals, pois a mensagem que retorna compara a frase mais o ícone de fechamento 'x' da janela
+    // Usei o assertTrue e o contains, ao inves do assertEquals, pois a mensagem de retorno compara a frase mais o ícone de fechamento 'x' da janela
 
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Espera o tempo que foi configurado
     WebElement mensagem = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("flash"))); // Espera até o elemento aparecer na tela
     String mensagemRetorno = mensagem.getText().trim(); // o trim ignora os espaços em branco
     Assertions.assertTrue(mensagemRetorno.contains(mensagemEsperada));
-
 }
 
 @Test
@@ -68,7 +56,7 @@ public void LoginUsuarioIncorreto() {
     driver.get("https://the-internet.herokuapp.com/login");
     driver.manage().window().setSize(new Dimension(1520, 840));
     driver.findElement(By.id("username")).click();
-    driver.findElement(By.id("username")).sendKeys("Tomsmith"); //senha incorreta
+    driver.findElement(By.id("username")).sendKeys("Tomsmith"); //usuario incorreto
     driver.findElement(By.id("password")).click();
     driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
     driver.findElement(By.cssSelector("i.fa.fa-2x.fa-sign-in")).click();
@@ -86,9 +74,9 @@ public void LoginSenhaIncorreta() {
     driver.get("https://the-internet.herokuapp.com/login");
     driver.manage().window().setSize(new Dimension(1520, 840));
     driver.findElement(By.id("username")).click();
-    driver.findElement(By.id("username")).sendKeys("tomsmith"); //usuario correto
+    driver.findElement(By.id("username")).sendKeys("tomsmith"); 
     driver.findElement(By.id("password")).click();
-    driver.findElement(By.id("password")).sendKeys("supersecretpassword");
+    driver.findElement(By.id("password")).sendKeys("supersecretpassword"); //senha incorreta
     driver.findElement(By.cssSelector("i.fa.fa-2x.fa-sign-in")).click();
 
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
