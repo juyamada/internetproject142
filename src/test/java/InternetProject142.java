0361c3aa845d52e14eed1ed57450a683888727ba
juyamada <jysyamada@gmail.com>
2024-11-06 11:55:20 -0300

    teste SenhaEmBranco

--- src/test/java/InternetProject142.java
+++ src/test/java/InternetProject142.java
@@ -133,8 +133,25 @@ public void LoginUsuarioEmBranco() {
     String mensagemRetorno = mensagem.getText().trim();
     Assertions.assertTrue(mensagemRetorno.contains(mensagemEsperada));
 }
+@Test
+public void LoginSenhaEmBranco() {
 
-//LoginUsuarioEmBranco
+    String mensagemEsperada = "Your password is invalid!";
+
+    driver.get("https://the-internet.herokuapp.com/login");
+    driver.manage().window().setSize(new Dimension(1520, 840));
+    driver.findElement(By.id("username")).click();
+    driver.findElement(By.id("username")).sendKeys("tomsmith"); 
+    driver.findElement(By.id("password")).click();
+    driver.findElement(By.id("password")).sendKeys("");  //senha em branco
+    driver.findElement(By.cssSelector("i.fa.fa-2x.fa-sign-in")).click();
+
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    WebElement mensagem = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("flash")));
+    String mensagemRetorno = mensagem.getText().trim();
+    Assertions.assertTrue(mensagemRetorno.contains(mensagemEsperada));
+}
+
 //LoginSenhaEmBranco
 //LoginUsuarioESenhaEmBranco
 //LoginEspacoEmBrancoUsuario
