0a195f567fc23131143c95438a14df6421f852d6
juyamada <jysyamada@gmail.com>
2024-11-06 11:24:00 -0300

    teste loginUsuarioIncorreto

--- src/test/java/InternetProject142.java
+++ src/test/java/InternetProject142.java
@@ -52,15 +52,41 @@ public void LoginComSucesso() {
     driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
     driver.findElement(By.cssSelector("i.fa.fa-2x.fa-sign-in")).click();
 
-
     // Usei o assertTrue e o contains, ao inves do assertEquals, pois a mensagem que retorna compara a frase mais o ícone de fechamento 'x' da janela
 
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Espera o tempo que foi configurado
     WebElement mensagem = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("flash"))); // Espera até o elemento aparecer na tela
     String mensagemRetorno = mensagem.getText().trim(); // o trim ignora os espaços em branco
-    Assertions.assertTrue(mensagemRetorno.contains(mensagemEsperada),"O login não foi realizado com sucesso");
+    Assertions.assertTrue(mensagemRetorno.contains(mensagemEsperada));
 
 }
+
+@Test
+public void LoginUsuarioIncorreto() {
+    String mensagemEsperada = "Your username is invalid!";
+
+    driver.get("https://the-internet.herokuapp.com/login");
+    driver.manage().window().setSize(new Dimension(15520, 840));
+    driver.findElement(By.id("username")).click();
+    driver.findElement(By.id("username")).sendKeys("Tomsmith"); //senha incorreta
+    driver.findElement(By.id("password")).click();
+    driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+    driver.findElement(By.cssSelector("i.fa.fa-2x.fa-sign-in")).click();
+
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    WebElement mensagem  = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("flash")));
+    String mensagemRetorno = mensagem.getText().trim();
+    Assertions.assertTrue(mensagemRetorno.contains(mensagemEsperada));
+}
+
+//LoginSenhaIncorreta
+//LoginUsuarioESenhaIncorretos
+//LoginUsuarioEmBranco
+//LoginSenhaEmBranco
+//LoginUsuarioESenhaEmBranco
+//LoginEspacoEmBrancoUsuario
+//LoginEspacoEmBrancoSenha
+
 
 
 }
